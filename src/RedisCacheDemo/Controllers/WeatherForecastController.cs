6895743b1ff1fe2d667803c4f2aa95465b7b4ad0
bezzad <behzad.khosravifar@gmail.com>
2023-03-06 15:44:18 +0330

    Added other API methods

--- src/RedisCacheDemo/Controllers/WeatherForecastController.cs
+++ src/RedisCacheDemo/Controllers/WeatherForecastController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using RedisCacheDemo;
 using RedisCacheDemo.Cache;
 
 namespace RedisCacheDemo.Controllers
@@ -32,7 +31,6 @@ namespace RedisCacheDemo.Controllers
                 return cacheData;
             }
 
-            var expirationTime = DateTimeOffset.Now.AddMinutes(50.0);
             cacheData = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
@@ -40,8 +38,54 @@ namespace RedisCacheDemo.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             }).ToArray();
 
-            _cacheService.SetData(nameof(WeatherForecast), cacheData, expirationTime);
+            Save(cacheData, 50);
             return cacheData;
+        }
+
+        [HttpGet(nameof(WeatherForecast))]
+        public WeatherForecast Get(int id)
+        {
+            WeatherForecast filteredData;
+            var cacheData = _cacheService.GetData<IEnumerable<WeatherForecast>>(nameof(WeatherForecast));
+            if (cacheData != null)
+            {
+                filteredData = cacheData.Where(x => x.Id == id).FirstOrDefault();
+                return filteredData;
+            }
+
+            cacheData = Get();
+            return cacheData.Where(w => w.Id == id).FirstOrDefault();
+        }
+
+        [HttpPost("addWeatherForecast")]
+        public async Task<WeatherForecast> Post(WeatherForecast value)
+        {
+            var cacheData = Get().ToList();
+            cacheData.Add(value);
+            Save(cacheData);
+            return value;
+        }
+
+        [HttpPut("updateWeatherForecast")]
+        public void Put(WeatherForecast WeatherForecast)
+        {
+            var cacheData = Get().ToDictionary(key => key.Id, val => val);
+            cacheData[WeatherForecast.Id] = WeatherForecast;
+            Save(cacheData.Values);
+        }
+
+        [HttpDelete("deleteWeatherForecast")]
+        public void Delete(int id)
+        {
+            var cacheData = Get().ToDictionary(key => key.Id, val => val);
+            cacheData.Remove(id);
+            Save(cacheData.Values);
+        }
+
+        private void Save(IEnumerable<WeatherForecast> weatherForecasts, double expireAfterMinutes = 50)
+        {
+            var expirationTime = DateTimeOffset.Now.AddMinutes(expireAfterMinutes);
+            _cacheService.SetData(nameof(WeatherForecast), weatherForecasts, expirationTime);
         }
     }
 }
--- src/RedisCacheDemo/WeatherForecast.cs
+++ src/RedisCacheDemo/WeatherForecast.cs
@@ -2,6 +2,8 @@ namespace RedisCacheDemo
 {
     public class WeatherForecast
     {
+        public int Id { get; set; }
+
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
