40c59feafc48ccdffb8d0f6696e1e1ea7c73afb3
bezzad <behzad.khosravifar@gmail.com>
2023-04-15 10:32:33 +0330

    update: splitting add and get method to separeted methods

--- src/RedisCache.Benchmark/BenchmarkManager.cs
+++ src/RedisCache.Benchmark/BenchmarkManager.cs
@@ -14,8 +14,11 @@ namespace RedisCache.Benchmark
         IMemoryCache _memCache;
         ICacheService _redisCache;
         const string KeyPrefix = "test_";
+        const string ReadKeyPrefix = "test_x";
         const int ExpireDurationSecond = 3600;
-        SampleModel[] _data;
+        static SampleModel[] _data;
+        static Lazy<SampleModel> _singleModel = new Lazy<SampleModel>(() => _data[0], true);
+        static Lazy<SampleModel> _singleWorseModel = new Lazy<SampleModel>(() => _data[1], true);
 
         [Params(1, 10, 100)]
         public int RepeatCount { get; set; }
@@ -27,98 +30,142 @@ namespace RedisCache.Benchmark
             var connection = ConnectionMultiplexer.Connect("127.0.0.1:6379");
             _redisCache = new CacheService(connection);
             _memCache = new MemoryCache(new MemoryCacheOptions());
-            _data = Enumerable.Range(0, 10000).Select(_ => SampleModel.Factory()).ToArray();
+            _data ??= Enumerable.Range(0, 10000).Select(_ => SampleModel.Factory()).ToArray();
         }
 
         [Benchmark(Baseline = true)]
-        public void AddGet_Memory()
+        public void Add_Memory()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 _memCache.Set(KeyPrefix + i, _data[i], DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
-
-            // read cache
-            for (var i = 0; i < RepeatCount; i++)
-                if (_memCache.TryGetValue(KeyPrefix + i, out SampleModel value))
-                    ThrowIfIsNotMatch(value, i);
         }
 
         [Benchmark]
-        public async Task AddGet_Memory_Async()
+        public async Task Add_Memory_Async()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 await _memCache.GetOrCreateAsync(KeyPrefix + i, _ => Task.FromResult(_data[i]));
-
-
-            // read cache
-            for (var i = 0; i < RepeatCount; i++)
-            {
-                var value = await _memCache.GetOrCreateAsync(KeyPrefix + i, _ => Task.FromResult(_data[i]));
-                ThrowIfIsNotMatch(value, i);
-            }
         }
 
         [Benchmark]
-        public void AddGet_Redis()
+        public void Add_Redis()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 _redisCache.AddOrUpdate(KeyPrefix + i, _data[i], DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
-
-            // read cache
-            for (var i = 0; i < RepeatCount; i++)
-                if (_redisCache.TryGetValue(KeyPrefix + i, out SampleModel value))
-                    ThrowIfIsNotMatch(value, i);
         }
 
         [Benchmark]
-        public async Task AddGet_Redis_Async()
+        public async Task Add_Redis_Async()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 await _redisCache.AddOrUpdateAsync(KeyPrefix + i, _data[i], DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
-
-            // read cache
-            for (var i = 0; i < RepeatCount; i++)
-            {
-                var value = await _redisCache.GetAsync(KeyPrefix + i, () => Task.FromResult(_data[i]), ExpireDurationSecond);
-                ThrowIfIsNotMatch(value, i);
-            }
         }
 
         [Benchmark]
-        public void AddGet_FireAndForget_Redis()
+        public void Add_FireAndForget_Redis()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 _redisCache.AddOrUpdate(KeyPrefix + i, _data[i], DateTimeOffset.Now.AddSeconds(ExpireDurationSecond), true);
-
-            // read cache
-            for (var i = 0; i < RepeatCount; i++)
-                if (_redisCache.TryGetValue(KeyPrefix + i, out SampleModel value))
-                    ThrowIfIsNotMatch(value, i);
         }
 
         [Benchmark]
-        public async Task AddGet_FireAndForget_Redis_Async()
+        public async Task Add_FireAndForget_Redis_Async()
         {
             // write cache
             for (var i = 0; i < RepeatCount; i++)
                 await _redisCache.AddOrUpdateAsync(KeyPrefix + i, _data[i], DateTimeOffset.Now.AddSeconds(ExpireDurationSecond), true);
+        }
+
+        [Benchmark]
+        public void Get_Memory()
+        {
+            // write single cache
+            _memCache.Set(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
+
+            // read cache
+            for (var i = 0; i < RepeatCount; i++)
+                if (_memCache.TryGetValue(ReadKeyPrefix, out SampleModel value))
+                    ThrowIfIsNotMatch(value, _singleModel.Value);
+        }
+
+        [Benchmark]
+        public async Task Get_Memory_Async()
+        {
+            // write single cache
+            _memCache.Set(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
 
             // read cache
             for (var i = 0; i < RepeatCount; i++)
             {
-                var value = await _redisCache.GetAsync(KeyPrefix + i, () => Task.FromResult(_data[i]), ExpireDurationSecond);
-                ThrowIfIsNotMatch(value, i);
+                // don't generate correct data when couldn't find, because its already wrote!
+                var value = await _memCache.GetOrCreateAsync(ReadKeyPrefix, _ => Task.FromResult(_singleWorseModel.Value));
+                ThrowIfIsNotMatch(value, _singleModel.Value);
             }
         }
 
-        private void ThrowIfIsNotMatch(SampleModel a, int index)
+        [Benchmark]
+        public void Get_Redis()
         {
-            if (a.Id != _data[index].Id)
-                throw new ArrayTypeMismatchException($"value.Id({a.Id} not equal with _data[{index}].Id({_data[index].Id}");
+            // write single cache
+            _redisCache.AddOrUpdate(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
+
+            // read cache
+            for (var i = 0; i < RepeatCount; i++)
+                if (_redisCache.TryGetValue(ReadKeyPrefix, out SampleModel value))
+                    ThrowIfIsNotMatch(value, _singleModel.Value);
+        }
+
+        [Benchmark]
+        public async Task Get_Redis_Async()
+        {
+            // write single cache
+            await _redisCache.AddOrUpdateAsync(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond));
+
+            // read cache
+            for (var i = 0; i < RepeatCount; i++)
+            {
+                // don't generate correct data when couldn't find, because its already wrote!
+                var value = await _redisCache.GetAsync(ReadKeyPrefix, () => Task.FromResult(_singleWorseModel.Value), ExpireDurationSecond);
+                ThrowIfIsNotMatch(value, _singleModel.Value);
+            }
+        }
+
+        [Benchmark]
+        public void Get_FireAndForget_Redis()
+        {
+            // write single cache
+            _redisCache.AddOrUpdate(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond), true);
+
+            // read cache
+            for (var i = 0; i < RepeatCount; i++)
+                if (_redisCache.TryGetValue(ReadKeyPrefix, out SampleModel value))
+                    ThrowIfIsNotMatch(value, _singleModel.Value);
+        }
+
+        [Benchmark]
+        public async Task Get_FireAndForget_Redis_Async()
+        {
+            // write single cache
+            await _redisCache.AddOrUpdateAsync(ReadKeyPrefix, _singleModel.Value, DateTimeOffset.Now.AddSeconds(ExpireDurationSecond), true);
+
+            // read cache
+            for (var i = 0; i < RepeatCount; i++)
+            {
+                // don't generate correct data when couldn't find, because its already wrote!
+                var value = await _redisCache.GetAsync(ReadKeyPrefix, () => Task.FromResult(_singleWorseModel.Value), ExpireDurationSecond);
+                ThrowIfIsNotMatch(value, _singleModel.Value);
+            }
+        }
+
+        private void ThrowIfIsNotMatch(SampleModel a, SampleModel b)
+        {
+            if (a?.Id != b?.Id)
+                throw new ArrayTypeMismatchException($"value.Id({a?.Id} not equal with _data[i].Id({b?.Id}");
         }
     }
 }
